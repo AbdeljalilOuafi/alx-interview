6dcc4d8593e21994d66c36dc1377adb7afea9f73
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-10-17 18:30:56 +0100

    dfoc

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
+"""0-minoperations module"""
+
+
 def minOperations(n):
+    """
+    Calculate the minimum number of operations needed
+    to achieve exactly n 'H' characters
+
+    Parameters:
+    n (int): The target number of 'H' characters to achieve.
+
+    Returns:
+    int: The minimum number of operations required to reach exactly n 'H'
+    """
     if n <= 1:
         return 0
 
